e28147f012aec1cf63fda51c47bf2dd9e15b779c
shiningsudipto <shiningsudipto@gmail.com>
2025-12-05 11:04:02 +0600

    calendly even tracking added

--- src/components/shared/CalendlyForm.tsx
+++ src/components/shared/CalendlyForm.tsx
@@ -12,6 +12,26 @@ const CalendlyForm = () => {
       document.body.removeChild(script);
     };
   }, []);
+
+  useEffect(() => {
+    const handleCalendlyEvent = (e: MessageEvent) => {
+      if (e.data.event && e.data.event.indexOf('calendly') === 0) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (window as any).dataLayer = (window as any).dataLayer || [];
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (window as any).dataLayer.push({
+          event: e.data.event,
+          payload: e.data
+        });
+      }
+    };
+
+    window.addEventListener('message', handleCalendlyEvent);
+
+    return () => {
+      window.removeEventListener('message', handleCalendlyEvent);
+    };
+  }, []);
   return (
     <div
       className="calendly-inline-widget"
